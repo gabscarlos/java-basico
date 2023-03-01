977b47f70abd838a01ccfb3438996699b2d1f0d9
Gabriel Carlos <gabrielcarllos@yahoo.com.br>
2023-03-01 14:32:18 -0300

    sobrecarga

---
+++ metodos/MAula02/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        System.out.println("Exercicio Quadrilatero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d, 5d);
+        Quadrilatero.area(7, 8, 9);
+        Quadrilatero.area(5f, 5f);
+    }
+}
